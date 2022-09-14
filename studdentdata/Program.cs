d6ba82034ec37c897aa484d815f44d21e33833cc
Mohan <gpmohan12345@gmail.com>
2022-09-14 21:33:26 +0530

    upload

---
+++ studdentdata/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace studentdata
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+           
+        {
+            //string data = @"D:\sort and search\studentdata.txt";
+            string data = File.ReadAllText("D:\\sort and search\\studentdata.txt");
+            Console.WriteLine(data);
+        }
+    }
+}
